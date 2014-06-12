d51197137a697f0c6be7fd298ed992520082fe87
rfeuer <rfeuer@hotmail.com>
2014-06-12 11:05:44 -0700

    new commit

---
+++ SimpleWCF/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SimpleWCF
+{
+    [DataContract]
+    public class Response
+    {
+        [DataMember]
+        public string Content { get; set; }
+    }
+}
---
+++ SimpleWCF/Service1.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SimpleWCF
+{
+    public class Service1 : IService1
+    {
+        public Response GetData(string value)
+        {
+            return new Response() { Content = string.Format("You entered: {0}", value) };
+        }
+
+    }
+}
